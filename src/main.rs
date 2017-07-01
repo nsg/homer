0d5f13dbeec8c321dc955d6fa14da73396095786
Stefan Berggren <nsg@nsg.cc>
2017-07-01 17:50:38 +0200

    Add the design from old autohome.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
+extern crate rocket_contrib;
 extern crate rocket;
 
+use rocket_contrib::Template;
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    let context = {};
+    Template::render("index", &context)
 }
 
 fn main() {
