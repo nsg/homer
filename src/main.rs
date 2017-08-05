a9353b22a3ba7549ca0131caef38e25fb9e6c338
Stefan Berggren <nsg@nsg.cc>
2017-08-05 18:14:08 +0200

    Prints the cmd, token from env and returns json.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
-extern crate rocket_contrib;
+#[macro_use] extern crate rocket_contrib;
 extern crate rocket;
 
 use rocket_contrib::Template;
+use rocket_contrib::Json;
+use std::env;
 
 #[get("/")]
 fn index() -> Template {
@@ -12,6 +14,24 @@ fn index() -> Template {
     Template::render("index", &context)
 }
 
+#[get("/<cmd>")]
+fn api_hue(cmd: String) -> Json {
+    let token = match env::var("HUE_TOKEN") {
+        Ok(v) => v,
+        _ => return Json(json!({"error": "Token not found"}))
+    };
+
+    Json(json!({
+        "command": cmd,
+        "token": token
+    }))
+}
+
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).attach(Template::fairing()).launch();
+    rocket::ignite()
+        .mount("/", routes![index])
+        .mount("/api/hue/", routes![api_hue])
+        .attach(Template::fairing())
+        .launch();
 }
